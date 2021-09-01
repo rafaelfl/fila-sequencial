64575954ac317fa832aaca3bec7579cd04d4869e
Rafael Fernandes Lopes <31193433+rafaelfl@users.noreply.github.com>
2021-09-01 01:03:26 +0000

    Versão em que o desenfileiramento é feito copiando os elementos para a posição anterior. O desempenho desse algoritmo é ruim!

--- fila.c
+++ fila.c
@@ -34,13 +34,23 @@ int enfileirar(Fila *f, char valor) {
 }
 
 char desenfileirar (Fila *f) {
+  int i = 0;
+
   // existe algo para desenfileirar
   if (f->ini < f->fim) {
     int ini_da_fila = f->ini;
+    int fim_da_fila = f->fim;
 
+    // guardo uma copia do inicio da fila
     char c = f->v[ini_da_fila];
 
-    f->ini++;
+    // copio cada um dos elementos uma posição para trás (substituindo o inicio)
+    for (i = ini_da_fila+1; i < fim_da_fila; i++) {
+      f->v[i-1] = f->v[i];
+    }
+
+    // como eu removi um elemento, decremento o fim da fila
+    f->fim--;
 
     return c;
   } else {
--- main.c
+++ main.c
@@ -8,95 +8,7 @@ int main(void) {
 
   imprimir_fila(f);
 
-  // if (esta_vazia(f)) {
-  //   printf("VAZIA\n");
-  // } else {
-  //   printf("não VAZIA\n");
-  // }
-
-  // if (esta_cheia(f)) {
-  //   printf("CHEIA\n");
-  // } else {
-  //   printf("não CHEIA\n");
-  // }
-
-  // enfileirar(f, 'X');
-  // imprimir_fila(f);
-
-  // enfileirar(f, 'L');
-  // imprimir_fila(f);
-
-  // enfileirar(f, 'R');
-  // imprimir_fila(f);
-
-  // if (esta_vazia(f)) {
-  //   printf("VAZIA\n");
-  // } else {
-  //   printf("não VAZIA\n");
-  // }
-
-  // if (esta_cheia(f)) {
-  //   printf("CHEIA\n");
-  // } else {
-  //   printf("não CHEIA\n");
-  // }
-
-  // enfileirar(f, '8');
-  // imprimir_fila(f);
-
-  // enfileirar(f, 'S');
-  // imprimir_fila(f);
-
-  // if (esta_vazia(f)) {
-  //   printf("VAZIA\n");
-  // } else {
-  //   printf("não VAZIA\n");
-  // }
-
-  // if (esta_cheia(f)) {
-  //   printf("CHEIA\n");
-  // } else {
-  //   printf("não CHEIA\n");
-  // }
-
-  // printf("===> %c\n", desenfileirar(f));
-  // imprimir_fila(f);
-
-  // printf("===> %c\n", desenfileirar(f));
-  // imprimir_fila(f);
-
-  // printf("===> %c\n", desenfileirar(f));
-  // imprimir_fila(f);
-
-  // printf("===> %c\n", desenfileirar(f));
-  // imprimir_fila(f);
-
-  // printf("===> %c\n", desenfileirar(f));
-  // imprimir_fila(f);
-
-  // printf("===> %c\n", desenfileirar(f));
-  // imprimir_fila(f);
-
-  // if (esta_vazia(f)) {
-  //   printf("VAZIA\n");
-  // } else {
-  //   printf("não VAZIA\n");
-  // }
-
-  // if (esta_cheia(f)) {
-  //   printf("CHEIA\n");
-  // } else {
-  //   printf("não CHEIA\n");
-  // }
-
-  enfileirar(f, 'X');
-  enfileirar(f, 'L');
-  imprimir_fila(f);
-  desenfileirar(f);
-  desenfileirar(f);
-  imprimir_fila(f);
-
-   if (esta_vazia(f)) {
+  if (esta_vazia(f)) {
     printf("VAZIA\n");
   } else {
     printf("não VAZIA\n");
@@ -107,6 +19,94 @@ int main(void) {
   } else {
     printf("não CHEIA\n");
   }
+
+  enfileirar(f, 'X');
+  imprimir_fila(f);
+
+  enfileirar(f, 'L');
+  imprimir_fila(f);
+
+  enfileirar(f, 'R');
+  imprimir_fila(f);
+
+  if (esta_vazia(f)) {
+    printf("VAZIA\n");
+  } else {
+    printf("não VAZIA\n");
+  }
+
+  if (esta_cheia(f)) {
+    printf("CHEIA\n");
+  } else {
+    printf("não CHEIA\n");
+  }
+
+  enfileirar(f, '8');
+  imprimir_fila(f);
+
+  enfileirar(f, 'S');
+  imprimir_fila(f);
+
+  if (esta_vazia(f)) {
+    printf("VAZIA\n");
+  } else {
+    printf("não VAZIA\n");
+  }
+
+  if (esta_cheia(f)) {
+    printf("CHEIA\n");
+  } else {
+    printf("não CHEIA\n");
+  }
+
+  printf("===> %c\n", desenfileirar(f));
+  imprimir_fila(f);
+
+  printf("===> %c\n", desenfileirar(f));
+  imprimir_fila(f);
+
+  printf("===> %c\n", desenfileirar(f));
+  imprimir_fila(f);
+
+  printf("===> %c\n", desenfileirar(f));
+  imprimir_fila(f);
+
+  printf("===> %c\n", desenfileirar(f));
+  imprimir_fila(f);
+
+  printf("===> %c\n", desenfileirar(f));
+  imprimir_fila(f);
+
+  if (esta_vazia(f)) {
+    printf("VAZIA\n");
+  } else {
+    printf("não VAZIA\n");
+  }
+
+  if (esta_cheia(f)) {
+    printf("CHEIA\n");
+  } else {
+    printf("não CHEIA\n");
+  }
+
+  // enfileirar(f, 'X');
+  // enfileirar(f, 'L');
+  // imprimir_fila(f);
+  // desenfileirar(f);
+  // desenfileirar(f);
+  // imprimir_fila(f);
+
+  //  if (esta_vazia(f)) {
+  //   printf("VAZIA\n");
+  // } else {
+  //   printf("não VAZIA\n");
+  // }
+
+  // if (esta_cheia(f)) {
+  //   printf("CHEIA\n");
+  // } else {
+  //   printf("não CHEIA\n");
+  // }
 
   //   0  1. 2. 3  4.    5
   //   i     f                 
